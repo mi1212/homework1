60aa32fa194807ad48691bfd0a6323d06d8683cd
mi1212 <mc_gorba4ev@mail.ru>
2023-01-10 20:15:05 +0300

    added itemsSegmentControl

---
+++ UISegmentControll/UISegmentControll/ItemsViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  UISegmentControll
+//
+//  Created by Mikhail Chuparnov on 10.01.2023.
+//
+
+import UIKit
+
+class ItemsViewController: UIViewController {
+    
+    let itemsArray = ["one", "two", "three"]
+    let colorsArray = [
+        UIColor.systemBlue,
+        UIColor.cyan,
+        UIColor.lightGray
+    ]
+    private lazy var itemsSegmentControl: UISegmentedControl = {
+        let segmentControll = UISegmentedControl(items: itemsArray)
+        segmentControll.translatesAutoresizingMaskIntoConstraints = false
+        segmentControll.isMomentary = true
+        return segmentControll
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        self.view.addSubview(itemsSegmentControl)
+        
+        self.itemsSegmentControl.addTarget(self, action: #selector(selectValues), for: .valueChanged)
+        NSLayoutConstraint.activate([
+            itemsSegmentControl.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+            itemsSegmentControl.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+            itemsSegmentControl.heightAnchor.constraint(equalToConstant: 30),
+            itemsSegmentControl.widthAnchor.constraint(equalToConstant: 250),
+        ])
+    }
+    
+    @objc private func selectValues(target: UISegmentedControl) {
+        if target == self.itemsSegmentControl {
+            let segmentIndex = target.selectedSegmentIndex
+            self.view.backgroundColor = colorsArray[segmentIndex]
+        }
+    }
+    
+    
+}
+
